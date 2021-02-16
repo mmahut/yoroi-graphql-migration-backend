19394adec93bf1241fa9987df69fb5bed1e65fe6
Marek Mahut <marek.mahut@fivebinaries.com>
2021-02-16 12:10:29 +0100

    signedTransaction: the new endpoint accepts binary

--- src/services/signedTransaction.ts
+++ src/services/signedTransaction.ts
@@ -4,9 +4,6 @@ import { Request, Response } from "express";
 
 const submissionEndpoint :string = config.get("server.txSubmissionEndpoint");
 
-const contentTypeHeaders = {"Content-Type": "application/cbor"};
-
-
 export const handleSignedTx = async (req: Request, res: Response):Promise<void>=> { 
   if(!req.body.signedTx)
     throw new Error ("No signedTx in body");
@@ -15,8 +12,7 @@ export const handleSignedTx = async (req: Request, res: Response):Promise<void>=
   try {
     const endpointResponse = await axios({ method:"post"
       , url: submissionEndpoint
-      , data: buffer
-      , headers: contentTypeHeaders}); 
+      , data: req.body.signedTx);
     if(endpointResponse.status === 202){
       if(endpointResponse.data.Left){
         const msg = `Transaction was rejected: ${endpointResponse.data.Left}`;
